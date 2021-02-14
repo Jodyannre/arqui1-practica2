69ca81380b4ff5f22ddda3bdf6cbe5c186b32045
Edin Montenegro <2181781580101@ingenieria.usac.edu.gt>
2021-02-14 15:33:47 -0600

    CONTROL DE ALARMA AGREGADO

--- hex/sketch_feb12a/sketch_feb12a.ino
+++ hex/sketch_feb12a/sketch_feb12a.ino
@@ -3,35 +3,130 @@
 
 #include <LiquidCrystal.h>
 
-LiquidCrystal lcd(53,51,49,47,45,43);
+//pines de lcd
+const int rs = 53;
+const int enable = 51;
+const int d4 = 49;
+const int d3 = 47;
+const int d2 = 45;
+const int d1 = 43;
+//pines de keypad
+//filas
+const int f1 = 10;
+const int f2 = 9;
+const int f3 = 8;
+const int f4 = 7;
+//columnas
+const int c1 = 13;
+const int c2 = 12;
+const int c3 = 11;
+
+//pin led alarma
+
+const int ledPin = 2;
+
+
+//PASSWORD
+const String PASS = "202112";
+int ContadorPass = 0;
 
 const byte rows = 4; //four rows
 const byte cols = 3; //three columns
 char keys[rows][cols] = {
-  {'1','2','3'},
-  {'4','5','6'},
-  {'7','8','9'},
-  {'#','0','*'}
+  {'1', '2', '3'},
+  {'4', '5', '6'},
+  {'7', '8', '9'},
+  {'*', '0', '#'}
 };
+byte rowPins[rows] = {f1, f2, f3, f4}; //connect to the row pinouts of the keypad
+byte colPins[cols] = {c1, c2, c3}; //connect to the column pinouts of the keypad
+Keypad teclado = Keypad( makeKeymap(keys), rowPins, colPins, rows, cols );
 
+LiquidCrystal lcd(rs, enable, d4, d3, d2, d1);
 
-byte rowPins[rows]={10,9,8,7};
-byte colPins[cols]={13,12,11};
-Keypad pad = Keypad(makeKeymap(keys),rowPins,colPins,rows,cols);
 
 void setup() {
   Serial.begin(9600);
-  lcd.begin(16,2);
+  lcd.begin(16, 2);
   lcd.print("Lab arquí");
-  
+  pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.print("Lab Arqui");
-  char c = '\0';
-  String contra_pad = "";
-  int i = 0;
+  if (distancia(149)) {
+    inputPass();
+  }
 }
+
+
+void inputPass() {
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("INGRESE SU CONTRASENIA");
+  char c = '\0';
+  String pass_pad = "";
+  int contador_ingresopad = 0;
+  while (true) {
+    c = teclado.getKey();
+    if (c == '*') break;
+    if (c != NO_KEY) {
+      pass_pad += c;
+      lcd.setCursor(contador_ingresopad, 1);
+      lcd.print(c);
+      contador_ingresopad++;
+    }
+  }
+
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  if (pass_pad == PASS) {
+    lcd.print("BIENVENIDO A CASA ^_^");
+    //****AQUI SE PUEDE INSTANCIAR EL METODO PARA GIRAR MOTORO**///////////////
+  }
+  else {
+    ContadorPass++;
+    if (ContadorPass < 3) {
+      lcd.print("ERROR EN CONTRASEÑA");
+      delay(1000);
+    }
+    else {
+      //lcd.print("Acceso No Autorizado");
+      AlarmaLed();
+    }
+  }
+}
+
+bool distancia(int distancia) {
+  if (distancia >= 148) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+void AlarmaLed() {
+  int contadorSegundos = 20;
+  
+  while (true) {
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Acceso No Autorizado");
+    lcd.setCursor(0, 1);
+    lcd.print(contadorSegundos);
+    if(contadorSegundos == 0){
+      break;
+    }
+    digitalWrite(ledPin, HIGH);
+    delay(250);
+    digitalWrite(ledPin, LOW);
+    delay(250);
+    digitalWrite(ledPin, HIGH);
+    delay(250);
+    digitalWrite(ledPin, LOW);
+    delay(250);
+    contadorSegundos--;
+  }
+  ContadorPass = 20;
+}
+
